22d116d515cb3c6b90464ea604622b2e8c92f9da
Nikes <107383684+Nyctophilic-nikes@users.noreply.github.com>
2025-04-03 13:48:39 +0530

    home

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
